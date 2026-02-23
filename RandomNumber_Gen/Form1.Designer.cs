10aee0fd60f7f5d573a2fbc8fbd403cee16ea843
delan75 <delnamuluh@gmail.com>
2026-02-23 18:51:13 +0200

    Add project files.

---
+++ RandomNumber_Gen/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace RandomNumber_Gen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(271, 121);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(315, 22);
+            this.txtValue.TabIndex = 0;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(271, 163);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(315, 22);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(164, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter N: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(88, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Path(URL): ";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(495, 211);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 40);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.ForeColor = System.Drawing.Color.Red;
+            this.lblOutput.Location = new System.Drawing.Point(251, 87);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(127, 20);
+            this.lblOutput.TabIndex = 1;
+            this.lblOutput.Text = "You entered:  ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.txtValue);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RandomNumber_Gen/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RandomNumber_Gen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtValue.Text == null || txtValue.Text.Trim() == "")
+                {
+                    MessageBox.Show("Please enter a number");
+                    return;
+                }
+
+                if (txtUrl.Text == "")
+                {
+                    MessageBox.Show("Please enter a URL");
+                    return;
+                }
+
+                int n = int.Parse(txtValue.Text);
+
+                // Get the current directory where the app is running from (bin/Debug)
+                string currentDirectory = Environment.CurrentDirectory;
+
+                // Combine with the relative path to get the correct file path
+                string filePath = Path.Combine(currentDirectory, @"..\output\out.txt");
+
+                // Check if the directory exists
+                string directoryPath = Path.GetDirectoryName(filePath);
+                if (!Directory.Exists(directoryPath))
+                {
+                    // If directory doesn't exist, create it
+                    Directory.CreateDirectory(directoryPath);
+                }
+
+                Random random = new Random();
+
+                // Open the file for writing (it will create the file if it doesn't exist)
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        int randomNumber = random.Next(1000, 2001);
+                        writer.WriteLine(randomNumber);
+                    }
+                }
+
+                // Notify the user
+                MessageBox.Show($"Successfully generated {n} random numbers to the file: {filePath}");
+
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Access to the specified file path is denied. Try running the app as an administrator.");
+            }
+            catch (IOException ioEx)
+            {
+                MessageBox.Show($"I/O error occurred: {ioEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An unexpected error occurred: {ex.Message}");
+            }
+        }
+
+    }
+}
