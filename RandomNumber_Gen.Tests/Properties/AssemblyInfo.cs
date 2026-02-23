2ac08979acd1b01acd083231f63deb1a441e53ac
delan75 <delnamuluh@gmail.com>
2026-02-23 19:23:54 +0200

    unit for the logic

---
+++ RandomNumber_Gen.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RandomNumber_Gen.Tests")]
+[assembly: AssemblyDescription("Unit tests for Random Number Generator")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RandomNumber_Gen.Tests")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8edf4429-251a-416d-bb68-93f227191bcf")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
